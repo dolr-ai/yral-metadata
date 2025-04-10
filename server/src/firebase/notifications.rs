461c002e7e65fbdf13d7aaf6df3d24a1d2694881
komal-sai-yral <komalsai@gobazzinga.io>
2025-04-10 20:09:09 +0530

    Revert "chore: update firebase keys and ids to use env vars"
    This reverts commit 30871c170d4fcc2eeba2496afa069a111f62c8c5.

--- server/src/firebase/notifications.rs
+++ server/src/firebase/notifications.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::env;
 
 use reqwest::Client;
 
@@ -73,10 +72,7 @@ impl Firebase {
             .post(url)
             .header("Authorization", format!("Bearer {}", firebase_token))
             .header("Content-Type", "application/json")
-            .header(
-                "project_id",
-                env::var("GOOGLE_CLIENT_NOTIFICATIONS_PROJECT_ID").unwrap(),
-            )
+            .header("project_id", "hot-or-not-feed-intelligence")
             .header("access_token_auth", "true")
             .body(data)
             .send()
