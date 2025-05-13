5dec3360b16a809b10a9298f4112bc1108fe0c4b
JoeruCodes <joymedi8143@gmail.com>
2025-05-14 04:29:54 +0530

    refactor: streamline device registration process and improve error handling
    - Simplified the `register_device` function by removing unnecessary unwraps and enhancing error handling for metadata retrieval.
    - Consolidated logic for updating notification keys and registration tokens, ensuring clearer flow and better logging.
    - Removed the unused `get_notification_key_by_name` method from the Firebase module to reduce code clutter.

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -38,16 +38,6 @@ pub struct UserMetadata {
     pub notification_key: Option<NotificationKey>,
 }
 
-impl Default for UserMetadata {
-    fn default() -> Self {
-        Self {
-            user_canister_id: Principal::anonymous(),
-            user_name: String::new(),
-            notification_key: None,
-        }
-    }
-}
-
 impl TryFrom<UserMetadata> for Message {
     type Error = Error;
     fn try_from(value: UserMetadata) -> Result<Self, Self::Error> {
