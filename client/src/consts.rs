4e0a098399149964626f86d7fe7fc11949960429
naitik-makwana-yral <naitik@gobazzinga.io>
2026-04-21 19:25:24 +0530

    update url for testing

--- client/src/consts.rs
+++ client/src/consts.rs
@@ -1,1 +1,2 @@
-pub const DEFAULT_API_URL: &str = "https://metadata.yral.com/";
+// changing this currently for testing.
+pub const DEFAULT_API_URL: &str = "https://pr-83-yral-metadata.preview.yral.com/";
